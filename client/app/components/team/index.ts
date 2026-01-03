eb906868870febf9069a28b1e6a39ee955ae1562
Mishal-Alajmi <mishalajmi.dev@gmail.com>
2026-01-04 00:52:16 +0300

    feat(MAS-113): build Team dashboard page with components

---
+++ client/app/components/team/index.ts
@@ -0,0 +1,12 @@
+export {
+	PermissionGrid,
+	PermissionBadges,
+	formatPermission,
+	type PermissionAction,
+	type PermissionResource,
+	type AvailablePermissions,
+} from "./permission-grid";
+
+export { InviteMemberDialog } from "./invite-member-dialog";
+export { EditPermissionsDialog } from "./edit-permissions-dialog";
+export { TransferOwnershipDialog } from "./transfer-ownership-dialog";
